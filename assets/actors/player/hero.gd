eee3f76d371af151a5b08c2590a17bf6e819673f
Alexxx180 <alexx120702@gmail.com>
2024-10-12 22:08:09 +0300

    add borders to start and end of the level

--- assets/actors/player/hero.gd
+++ assets/actors/player/hero.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
-enum { SPEED = 1000, JUMP = 1300, GRAVITY = 3875 }
+enum { SPEED = 1000, JUMP = 2600, GRAVITY = 7750 } # JUMP = 1300, GRAVITY = 3875 }
+
+@onready var camera: Camera2D = $camera
 
 func _input(_event: InputEvent) -> void:
 	velocity.x = Input.get_axis("move_left", "move_right") * SPEED
