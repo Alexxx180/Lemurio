dd3a0ba05a722d09717a0685010d57a815cc9c3d
Alexxx180 <alexx120702@gmail.com>
2024-10-13 22:01:04 +0300

    springs fix: no animation node

--- assets/actors/interactables/bonuses/spring/spring.gd
+++ assets/actors/interactables/bonuses/spring/spring.gd
@@ -3,7 +3,6 @@ extends StaticBody2D
 const multiplier: float = 1.5
 
 @onready var touch: Node = $touch
-@onready var animation: AnimationPlayer = $animation
 
 func _ready() -> void:
 	touch.feedback.connect(_start)
